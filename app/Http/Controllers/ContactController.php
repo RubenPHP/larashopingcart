8c2b40b16ef60345cc552e36cb3a64d3396343a3
RubenPHP <ruben@indahouser.com>
2017-01-19 01:06:58 +0100

    Added basic and ugly form. Learn about restful on Laravel. New controller an view for contact form.

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function show()
+    {
+
+        return view('contact.show');
+    }
+
+    public function send()
+    {
+
+        return back();
+    }
+}
